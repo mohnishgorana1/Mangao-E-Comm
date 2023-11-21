60ea9f37dc11a907db717f4c247825c07428952a
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-11-21 23:10:12 +0530

    product reducer init

---
+++ src/reducers/products_reducer.js
@@ -0,0 +1,11 @@
+import {
+    GET_PRODUCTS_BEGIN
+} from '../actions'
+
+const products_reducer = (state, action) => {
+    // reducers if(action.type+ ///.....)
+
+    
+}
+
+export default products_reducer
