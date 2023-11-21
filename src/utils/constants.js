9ecb853d960b02532b52a8b08528447891aebe1c
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-11-21 23:35:22 +0530

    addd constants

---
+++ src/utils/constants.js
@@ -0,0 +1,23 @@
+// import { GiCompass, GiDiamondHard, GiStabbedNote } from 'react-icons/gi'
+
+export const links = [
+  {
+    id: 1,
+    text: 'home',
+    url: '/',
+  },
+  {
+    id: 2,
+    text: 'about',
+    url: '/about',
+  },
+  {
+    id: 3,
+    text: 'products',
+    url: '/products',
+  },
+]
+
+export const products_url = 'https://course-api.com/react-store-products'
+
+export const single_product_url = `https://course-api.com/react-store-single-product?id=`
