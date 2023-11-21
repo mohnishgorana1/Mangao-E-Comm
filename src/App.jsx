a384c15b83685d4494fa949b2a5637b66fc2604d
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-11-21 23:34:28 +0530

    routing defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import {
+  HomePage,
+  SingleProduct,
+  Checkout,
+  ErrorPage,
+  LoginPage,
+  RegisterPage,
+  CartPage,
+  Products,
+} from "./pages";
+import { Navbar, Sidebar } from "./components";
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <Router>
+      <section className="align-element py-10">
+        <Navbar />
+        <Sidebar />
+        <section className="py-2 my-5">
+          <Routes>
+            <Route path="/" exact element={<HomePage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/:id" element={<SingleProduct />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </section>
+      </section>
+    </Router>
   );
 }
 
