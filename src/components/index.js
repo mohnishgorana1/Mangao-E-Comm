6d6319cd4d956f65a3ce87eec5d1a7a624a4199b
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-11-23 11:47:25 +0530

    added files

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,15 @@
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
-
+import Filters from "./Filters";
+import Sort from "./Sort";
+import ProductCard from './ProductCard'
+import ProductListing from "./ProductListing";
 
 export {
     Navbar,
-    Sidebar
+    Sidebar,
+    Filters,
+    Sort,
+    ProductCard,
+    ProductListing,
 }
