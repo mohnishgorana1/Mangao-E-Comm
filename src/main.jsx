891ad910321b171b75522e21e10e9ca557cc28c6
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-11-23 11:47:09 +0530

    context addede

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ProductsProvider } from "./context/products_context.jsx";
+import { FilterProvider } from "./context/filter_context.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ProductsProvider>
+    <FilterProvider>
+      <App />
+    </FilterProvider>
+  </ProductsProvider>
+);
