61690ff1115d508f6ffcf6658bdf7670f59b0ae5
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-11-24 16:27:42 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,25 @@ import "./index.css";
 import { ProductsProvider } from "./context/products_context.jsx";
 import { FilterProvider } from "./context/filter_context.jsx";
 import { CartProvider } from "./context/cart_context.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { UserProvider } from "./context/user_context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+  <Auth0Provider
+    domain="dev-8t03uob4hhcs4upz.us.auth0.com"
+    clientId="XlRjMSRwuYfGwAVmUlrvNTO3aILAgR7i"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
 );
