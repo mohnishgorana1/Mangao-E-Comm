f20da7db349de8135033a131bf7f9f8bbfc7e9d7
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-11-24 16:29:25 +0530

    checkout page init

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,9 +1,27 @@
-
+import { useCartContext } from "../context/cart_context";
+import { Link } from "react-router-dom";
 
 function Checkout() {
+  const { cart } = useCartContext();
+
   return (
-    <div>Checkout</div>
-  )
+    <main className="w-full px-4">
+      <div className="flex flex-col items-center justify-center">
+        {cart.length < 1 ? (
+          <div className="flex flex-col items-center justify-center gap-y-5">
+            <h2 className="text-red-500 font-bold text-lg tracking-widest">Your cart is empty</h2>
+            <Link to="/products" className="btn">
+              fill it
+            </Link>
+          </div>
+        ) : (
+          <div className="">
+
+          </div>
+        )}
+      </div>
+    </main>
+  );
 }
 
-export default Checkout
+export default Checkout;
