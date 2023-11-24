3147717d099842c76957f570c7099c040a947938
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-11-24 16:28:28 +0530

    add login btn with functionlity with auth 0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
-import {LoginPage} from '../pages' 
+import { LoginPage } from "../pages";
+import { FaShoppingCart } from "react-icons/fa";
+import { useCartContext } from "../context/cart_context";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Navbar() {
+  const { total_items } = useCartContext();
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
+
   return (
     <div className="navbar bg-base-200 rounded-md">
       <div className="navbar-start">
@@ -36,25 +42,56 @@ function Navbar() {
             </li>
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">Mangao E-Commerce</a>
+        <Link
+          to="/"
+          className=" font-bold sm:text-xl sm:ml-5 text-primary sm:font-extrabold sm:tracking-widest sm:hover:scale-105 "
+        >
+          Mangao E-Commerce
+        </Link>
       </div>
 
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
+        <ul className="menu menu-horizontal px-1 flex gap-x-2">
           <li>
-            <Link to="/">Home</Link>
+            <NavLink to="/">Home</NavLink>
           </li>
           <li>
-            <Link to="products">Products</Link>
+            <NavLink to="products">Products</NavLink>
           </li>
-          <li>
-            <Link to="checkout">Checkout</Link>
-          </li>
+          {isAuthenticated && (
+            <li>
+              <NavLink to="checkout">Checkout</NavLink>
+            </li>
+          )}
         </ul>
       </div>
 
-      <div className="navbar-end">
-          <NavLink to='/login' className='btn' >Login/Register</NavLink>
+      <div className="navbar-end mr-3 flex gap-2">
+        {isAuthenticated && <h1 className="text-accent text-sm sm:text-lg ">{user.name}</h1>}
+
+        {!isAuthenticated ? (
+          <button className="btn" onClick={() => loginWithRedirect()}>
+            Login/Register
+          </button>
+        ) : (
+          <button
+            className="btn"
+            onClick={() =>
+              logout({ logoutParams: { returnTo: window.location.origin } })
+            }
+          >
+            Logout
+          </button>
+        )}
+
+        <Link to="/cart" className="flex items-center text-2xl hover:scale-110">
+          <span className="flex items-center relative">
+            <FaShoppingCart />
+            <span className="absolute top-[-10px] right-[-15px] w-[8px] h-[8px] flex items-center justify-center border bg-white text-base-300 rounded-badge text-sm p-[10px] font-bold text-center">
+              {total_items}
+            </span>
+          </span>
+        </Link>
       </div>
     </div>
   );
