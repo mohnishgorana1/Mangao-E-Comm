e073510b63c69f3295393caf084e473e99564f53
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-11-24 16:28:59 +0530

    file creatded

---
+++ src/pages/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+import { useAuth0 } from "@auth0/auth0-react"
+import { Navigate } from "react-router-dom"
+
+
+function PrivateRoute({children}) {
+    const { user } = useAuth0()
+    if(!user){
+        return <Navigate to='/' />
+    }
+    return children;
+}
+
+export default PrivateRoute
